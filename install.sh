5b91e3da3703cb7ce4f9dc9ffa44c38d2d397283
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-08-16 09:30:46 +0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 set -ex
 
+unset POSIXLY_CORRECT
+ghc --version || $(ghc-select ghc-7.10.3)
+
 stack install
 sudo /etc/init.d/supervisor stop
-sudo cp ~/.local/bin/karma-server /usr/local/bin/
+sleep 1
+sudo cp ~/.local/bin/ajk-lomake-server /usr/local/bin/
 sudo /etc/init.d/supervisor start
